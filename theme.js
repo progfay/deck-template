9005216f79a1c2c15ff8a914a4f7a309d89383f1
progfay <shun1856shun@gmail.com>
2020-05-10 01:08:00 +0900

    feat: custom style of deck

---
+++ theme.js
@@ -0,0 +1,37 @@
+const blue = '#0af'
+
+export const theme = {
+  fonts: {
+    body: '"Roboto Condensed", system-ui, sans-serif',
+    heading: '"Roboto Condensed", system-ui, sans-serif',
+    monospace: '"Roboto Mono", monospace'
+  },
+  colors: {
+    text: 'red',
+    background: '#000',
+    primary: blue,
+    pre: blue
+  },
+  fontWeights: {
+    heading: 600,
+    bold: 600
+  },
+  text: {
+    heading: {}
+  },
+  styles: {
+    Slide: {},
+    Header: {},
+    Footer: {},
+    pre: {
+      color: 'primary',
+      bg: 'darkgray'
+    },
+    code: {
+      color: 'primary'
+    }
+  },
+  components: {
+    // pre: StatelessFunctionalComponent
+  }
+}
