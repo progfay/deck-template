3d11b0d7a80af308a9ae839e67a865e42470a6bb
progfay <shun1856shun@gmail.com>
2020-05-13 23:36:39 +0900

    feat: add code-sufer support

--- theme.js
+++ theme.js
@@ -141,6 +141,39 @@ export default {
     },
     img: {
       maxWidth: '100%'
+    },
+    CodeSurfer: {
+      pre: {
+        color: 'text',
+        backgroundColor: 'background'
+      },
+      code: {
+        color: 'text',
+        backgroundColor: 'background'
+      },
+      tokens: {
+        'comment cdata doctype': {
+          fontStyle: 'italic'
+        },
+        'builtin changed keyword punctuation operator tag deleted string attr-value char number inserted': {
+          color: 'primary'
+        },
+        'line-number': {
+          opacity: 0.8
+        }
+      },
+      title: {
+        backgroundColor: 'background',
+        color: 'text'
+      },
+      subtitle: {
+        color: '#d6deeb',
+        backgroundColor: 'rgba(10,10,10,0.9)'
+      },
+      unfocused: {
+        // only the opacity of unfocused code can be changed
+        opacity: 0.1
+      }
     }
   },
   components: {
