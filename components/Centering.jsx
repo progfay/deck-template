23ec5710639eb9ab8c688adf4ef722aca737f404
progfay <shun1856shun@gmail.com>
2020-05-14 23:11:38 +0900

    feat: add <Centering> component

---
+++ components/Centering.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Centering = styled.div`
+  width: 100%;
+  height: auto;
+  min-height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+  vertical-align: middle;
+  word-break: break-word;
+`
