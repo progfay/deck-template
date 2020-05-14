0419316a4d9c0fed1950593dd56dffaa449c9af7
progfay <shun1856shun@gmail.com>
2020-05-14 22:33:56 +0900

    refactor: apply css with styled-components

--- components/CodeBlock.jsx
+++ components/CodeBlock.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
+import styled from 'styled-components'
 import { dracula, colors as draculaColors } from '../theme/dracula'
 import dimensions from '../theme/dimensions'
+
+const FileNameBox = styled.p`
+  position: absolute;
+  transform: translate(-${dimensions.xlarge}, -${dimensions.xxlarge});
+  max-width: 30%;
+  padding: ${dimensions.small} ${dimensions.large};
+  margin: 0;
+  font-size: 0.7em;
+  background-color: ${draculaColors.comment};
+  border-radius: ${dimensions.small} ${dimensions.zero};
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
 
 export const CodeBlock = props => {
   const { className: preClassName, children } = props
@@ -13,21 +27,7 @@ export const CodeBlock = props => {
     <Highlight {...defaultProps} code={code.trim()} language={language} theme={dracula}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <pre className={`${preClassName} ${className}`} style={{ ...style, ...(fileName ? { paddingTop: '60px' } : {}) }}>
-          {
-            fileName &&
-              <p style={{
-                position: 'absolute',
-                transform: `translate(-${dimensions.xlarge}, -${dimensions.xxlarge})`,
-                borderRadius: `${dimensions.small} ${dimensions.zero}`,
-                fontSize: '0.7em',
-                padding: `${dimensions.small} ${dimensions.large}`,
-                background: draculaColors.comment,
-                margin: 0
-              }}
-              >
-                {fileName}
-              </p>
-          }
+          {fileName && <FileNameBox> {fileName} </FileNameBox>}
           {
             tokens.map((line, i) => (
               <div key={i} {...getLineProps({ line, key: i })}>
