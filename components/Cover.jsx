147159ab14d9f9760aa7969aa49adc48ed076f0b
progfay <shun1856shun@gmail.com>
2020-10-16 01:26:16 +0900

    fix: fix <Cover> components

--- components/Cover.jsx
+++ components/Cover.jsx
@@ -1,29 +1,30 @@
 import React from 'react'
+import { Centering } from './Centering'
 import colors from '../theme/colors'
 
-export const Cover = ({ url }) => {
+export const Cover = ({ url }) => (
   <>
     <div style={{
-  width: "100vw",
-  height: "100vh",
-  position: "fixed",
-  top: 0,
-  left: 0,
-  backgroundColor: colors.black,
-}} />
+      width: "100vw",
+      height: "100vh",
+      position: "fixed",
+      top: 0,
+      left: 0,
+      backgroundColor: colors.black,
+    }} />
 
-<Centering>
+    <Centering>
 
-  <div style={{
-    width: "100vw",
-    height: "100vh",
-    position: "absolute",
-    backgroundImage: `url("${url}")`,
-    backgroundSize: "contain",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-  }} />
+      <div style={{
+        width: "100vw",
+        height: "100vh",
+        position: "absolute",
+        backgroundImage: `url("${url}")`,
+        backgroundSize: "contain",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }} />
 
-</Centering>
+    </Centering>
   </>
-}
+)
