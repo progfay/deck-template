fa04c216c0804d6aa4679b56cd96a158f0b7ec99
progfay <shun1856shun@gmail.com>
2020-10-16 01:15:00 +0900

    feat: add <Cover> components

---
+++ components/Cover.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import colors from '../theme/colors'
+
+export const Cover = ({ url }) => {
+  <>
+    <div style={{
+  width: "100vw",
+  height: "100vh",
+  position: "fixed",
+  top: 0,
+  left: 0,
+  backgroundColor: colors.black,
+}} />
+
+<Centering>
+
+  <div style={{
+    width: "100vw",
+    height: "100vh",
+    position: "absolute",
+    backgroundImage: `url("${url}")`,
+    backgroundSize: "contain",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  }} />
+
+</Centering>
+  </>
+}
